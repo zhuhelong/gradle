6be978610bf784e62a6fe634a37274fbd5edcec0
Paul Merlin <paul@gradle.com>
2019-10-09 16:27:33 +0200

    Uniformly use RepoScriptBlockUtil in Kotlin DSL cross-version tests
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/kotlin-dsl-tooling-builders/src/crossVersionTest/groovy/org/gradle/kotlin/dsl/tooling/builders/r60/KotlinDslScriptsModelCrossVersionSpec.groovy
+++ subprojects/kotlin-dsl-tooling-builders/src/crossVersionTest/groovy/org/gradle/kotlin/dsl/tooling/builders/r60/KotlinDslScriptsModelCrossVersionSpec.groovy
@@ -155,6 +155,7 @@ class KotlinDslScriptsModelCrossVersionSpec extends AbstractKotlinScriptModelCro
 
         given:
         withBuildSrc()
+        withDefaultSettings()
         buildFileKts << ""
 
         when:
@@ -229,15 +230,17 @@ class KotlinDslScriptsModelCrossVersionSpec extends AbstractKotlinScriptModelCro
                 }
             }
         """)
-        def settings = withSettings("""
-            buildscript {
-                dependencies {
-                    classpath(files("${escapeString(settingsJar)}"))
+        def settings = withDefaultSettings().tap {
+            append("""
+                buildscript {
+                    dependencies {
+                        classpath(files("${escapeString(settingsJar)}"))
+                    }
                 }
-            }
-            apply(from = "some.gradle.kts")
-            include("a", "b")
-        """)
+                apply(from = "some.gradle.kts")
+                include("a", "b")
+            """)
+        }
         def root = withBuildScript("""
             buildscript {
                 dependencies {
@@ -259,6 +262,7 @@ class KotlinDslScriptsModelCrossVersionSpec extends AbstractKotlinScriptModelCro
                 `kotlin-dsl`
             }
             buildscript {
+                $repositoriesBlock
                 dependencies {
                     classpath(files("${escapeString(bJar)}"))
                 }
