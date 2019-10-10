7e81bfb288d22d2c008283e482a4bd4f487033dd
Paul Merlin <paul@gradle.com>
2019-10-10 11:13:56 +0200

    Revert unneeded changes in tests
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/kotlin-dsl-tooling-builders/src/crossVersionTest/groovy/org/gradle/kotlin/dsl/tooling/builders/r60/KotlinDslScriptsModelCrossVersionSpec.groovy
+++ subprojects/kotlin-dsl-tooling-builders/src/crossVersionTest/groovy/org/gradle/kotlin/dsl/tooling/builders/r60/KotlinDslScriptsModelCrossVersionSpec.groovy
@@ -155,7 +155,6 @@ class KotlinDslScriptsModelCrossVersionSpec extends AbstractKotlinScriptModelCro
 
         given:
         withBuildSrc()
-        withDefaultSettings()
         buildFileKts << ""
 
         when:
@@ -230,17 +229,15 @@ class KotlinDslScriptsModelCrossVersionSpec extends AbstractKotlinScriptModelCro
                 }
             }
         """)
-        def settings = withDefaultSettings().tap {
-            append("""
-                buildscript {
-                    dependencies {
-                        classpath(files("${escapeString(settingsJar)}"))
-                    }
+        def settings = withSettings("""
+            buildscript {
+                dependencies {
+                    classpath(files("${escapeString(settingsJar)}"))
                 }
-                apply(from = "some.gradle.kts")
-                include("a", "b")
-            """)
-        }
+            }
+            apply(from = "some.gradle.kts")
+            include("a", "b")
+        """)
         def root = withBuildScript("""
             buildscript {
                 dependencies {
@@ -262,7 +259,6 @@ class KotlinDslScriptsModelCrossVersionSpec extends AbstractKotlinScriptModelCro
                 `kotlin-dsl`
             }
             buildscript {
-                $repositoriesBlock
                 dependencies {
                     classpath(files("${escapeString(bJar)}"))
                 }
