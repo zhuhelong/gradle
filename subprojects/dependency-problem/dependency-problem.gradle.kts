63e330d59def9d9ab103a262c1762d514fdf66b5
Sam Snyder <sam@gradle.com>
2020-01-21 13:27:10 -0800

    Allow the build to be failed by passing -Pfail on the command line

--- subprojects/dependency-problem/dependency-problem.gradle.kts
+++ subprojects/dependency-problem/dependency-problem.gradle.kts
@@ -10,8 +10,15 @@ repositories {
     }
 }
 
+// The repo folder contains a full entry for a 6.1 version and a damaged/incomplete entry for 6.2
+val requestedVersion = if(project.hasProperty("fail")) {
+    "6.2+"
+} else {
+    "6.2.1"
+}
+
 dependencies {
-    runtimeOnly("org.gradle:dependencyProblem:6.2+")
+    runtimeOnly("org.gradle:example-dependency:$requestedVersion")
 }
 
 gradlebuildJava {
